90b7db97ab8b83712898718028529d7fdfb68b2a
Ignacio <ignacio.araya.11@alumnos.usm.cl>
2017-05-13 13:46:47 -0300

    test

--- quora_competition/test.sh
+++ quora_competition/test.sh
@@ -1,4 +1,4 @@
 #PBS -l walltime=2:00:00
 
 use anaconda3
-python /user/i/iaraya/Mazhine/quora_competition/lectura_de_datos.py 
+python /user/i/iaraya/files/Mazhine/quora_competition/lectura_de_datos.py 
