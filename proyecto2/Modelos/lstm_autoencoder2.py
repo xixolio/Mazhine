3a3332d90334b852bbfbab6f90a9749216f9d784
Ignacio <ignacio.araya.11@alumnos.usm.cl>
2017-05-24 00:05:10 -0300

    nuevo ae

---
+++ proyecto2/Modelos/lstm_autoencoder2.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 17:17:02 2017
+lstm_autoencoders is going to provide 2 ways to train an autoencoder. The main
+objetive for this is to test which one is capable of better reconstructing
+the inicial input so that we later on use that one to pretrain a lstm on the
+task of wind prediction
+
+The first type of autoencoder is going to have just one final output, the input
+of the last vector fed to the lstm, meaning that only the last hidden activation
+will be a lower dimensional representation of the input with all the meaningful
+features needed to reconstruct it.
+
+The second type is going to have multiple outputs, one for each time-step. Each
+output then equals the input at that time-step, meaning that each hidden state
+will be forced to be a hidden representation of its input.
+@author: ignacio
+"""
+from keras.models import Model
+from keras.layers import Dense,LSTM,Input
+
+
+
+def LSTM_Autoencoder2(tr_data,layers):
+    features = tr_data.shape[2]
+    time_steps = tr_data.shape[1]
+    l = len(layers)
+    
+    inputs = Input(shape=(time_steps,features))
+    dummy=inputs
+    for i in range(len(layers)-1):
+        dummy = LSTM(layers[i],activation='sigmoid',
+                     return_sequences=True,dropout=0.1)(dummy)
+    encoded = LSTM(layers[-1],activation='sigmoid',
+                   return_sequences=False,dropout=0.1)(dummy)
+    
+    dummy = encoded
+    for i in range(len(layers)-1):
+        dummy = Dense(layers[i-2],activation='sigmoid')(dummy)
+    decoded = Dense(features,activation='linear')(dummy)
+    
+    autoencoder = Model(inputs=inputs, outputs = decoded)
+    
+    encoder = Model(inputs=inputs, outputs = encoded)
+    
+    decoder_inputs = Input(shape=(time_steps,layers[-1]))
+    dummy = decoder_inputs
+    for i in range(len(layers)):
+        dummy = autoencoder.layers[-l+i](dummy)
+    decoder = Model(inputs=decoder_inputs, outputs=dummy)
+    
+    autoencoder.compile(optimizer='adadelta',loss='mse')
+    
+    #we get the model for the final prediction
+    inputs = Input(batch_shape=(1,time_steps,features))
+    dummy=inputs
+    for i in range(len(layers)-1):
+        dummy = LSTM(layers[i],activation='sigmoid',
+                     return_sequences=True,stateful=True)(dummy)
+    encoded = LSTM(layers[-1],activation='sigmoid',
+                   return_sequences=False,stateful=True)(dummy)
+    model = Model(inputs=inputs, outputs = encoded)
+    model.compile(optimizer='adadelta',loss='mse')
+ 
+    return autoencoder,encoder,decoder,model
+    
---
+++ proyecto2/cv3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 20:30:44 2017
+
+@author: ignacio
+"""
+
+import os
+import numpy as np
+from keras.callbacks import EarlyStopping
+from keras.models import Model
+from keras.layers import Dense
+import sys
+sys.path.insert(0,'/user/i/iaraya/Mazhine/proyecto2/Modelos')
+from lstm_autoencoder2 import LSTM_Autoencoder2
+from data_processing import data_processing
+
+
+
+if __name__ == "__main__":
+
+    lag = int(sys.argv[1])
+    time_steps = int(sys.argv[2])
+    layer = int(sys.argv[3])
+    real_data = np.loadtxt(open('/user/i/iaraya/Mazhine/proyecto2/serie1.b08C2.csv',"rb"),delimiter=",",skiprows=1,usecols=range(1,5))
+    maxv,minv,sets,outs = data_processing(real_data,lag,time_steps,10,5*30*24)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+    error = 0
+    for j in range(10):
+        tr_set = sets[j][:-24,:,:]
+        ts_set = sets[j][-24:,:,:]
+        tr_out = outs[j][:-24,:]
+        ts_out = outs[j][-24:,:]
+        autoencoder,encoder,decoder,model = LSTM_Autoencoder2(tr_set,[layer])
+        autoencoder.fit(tr_set,tr_set[:,-1,:],validation_split=0.2,callbacks=[early_stopping],epochs=1000,verbose=0)
+        error += np.mean(np.abs(ts_set[:,-1,:] - autoencoder.predict(ts_set)))
+
+    f=open("lstm_ae_recons_dr01.txt","a")
+    f.write(str(lag)+" "+str(time_steps)+" "+str(layer)+" ")
+    f.write(str(np.mean(error))+"\n")
+    f.close()
